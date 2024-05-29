293eccebb575dbdba4d343fd47e02aadea49cc28
Leo Irakliotis <lgreco@gmail.com>
2024-05-29 14:06:00 +0000

    Homework commit

---
+++ Assignments/Assignment 01/A01.java
@@ -0,0 +1,21 @@
+/*
+ * Use the starter code below to write a program that has variables for the following
+ * information:
+ *  - your first name,
+ *  - your favorite movie,
+ *  - your hometown
+ * Then use the variables above to produce the following output with the underlined 
+ * parts indicating data specific to you, stored in the variables you created.
+ *   
+ * Hello, my name is Frodo, I am from Hobbiton, and my favorite movie is Young Frankenstein
+ *                   -----            --------                           ------------------
+ * 
+ * To save this assignment, make sure you commit your changes to your GitHub repository,
+ * following the instructions in Sakai. IF YOU DO NOT COMMIT THE CHANGES, IT IS POSSIBLE THAT
+ * YOUR WORK MAY BE LOST AND YOU MAY HAVE TO START ALL OVER AGAIN.
+ */
+public class A01 {
+    public static void main(String[] args) {
+        // YOUR CODE HERE
+    }
+}
---
+++ Assignments/Assignment 01/readme.md
@@ -0,0 +1,3 @@
+# Assignment 01
+
+Use the starter code in A01.java to complete this assignment.
