259a49aa99bef38901e5c72b664cc30003753d70
Leo Irakliotis <lgreco@gmail.com>
2024-06-05 19:31:38 +0000

    Assignment 02

--- Assignments/Assignment 02/Decompose.java
+++ Assignments/Assignment 02/Decompose.java
@@ -1,3 +1,10 @@
+/*
+ * Assignment 02
+ * 
+ * Reorganize the code below so that there will no print statements in the 
+ * main method. Instead, main should contain only 4-5 lines of code.
+ * 
+ */
 public class Decompose {
     public static void main(String[] args) {
         System.out.println("3 ripe bananas");
